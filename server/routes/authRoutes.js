4622d9bb7b0a0950de25eaf9720dce9cf6783a72
Daudi Joseph <bright.dramx@gmail.com>
2025-04-25 02:40:17 +0300

    ✅ Add user registration with MySQL and frontend form

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const db = require('../server/db');
+const db = require('../db');
 const router = express.Router();
 
 // Register
